d931d4e3237ea8492befc9c3f8ccf133be009458
Adam Aposhian <aposhian.dev@gmail.com>
2021-11-15 19:59:41 -0700

    fixed max_toi bug

--- src/pathfinding.rs
+++ src/pathfinding.rs
@@ -32,13 +32,15 @@ pub struct Path {
     points: Vec<Vec2>
 }
 
-const MAX_TOI: f32 = 100.0;
+pub struct PathVisualization(Entity);
 
-const TOI_SCALE: i32 = 100;
+const MAX_TOI: f32 = 1.0;
 
 const THETA_STEPS: u8 = 8;
 
-#[derive(PartialEq, Eq, Hash, Clone, Copy)]
+const GRID_SCALE: u8 = 10;
+
+#[derive(PartialEq, Eq, Hash, Clone, Copy, Debug)]
 struct GridPoint(i32, i32);
 
 impl Add for GridPoint {
@@ -51,14 +53,14 @@ impl Add for GridPoint {
 
 impl From<Vec2> for GridPoint {
     fn from(value: Vec2) -> Self {
-        let rounded = value.round();
+        let rounded = (GRID_SCALE as f32 * value).round();
         GridPoint(rounded.x as i32, rounded.y as i32)
     }
 }
 
 impl Into<Vec2> for GridPoint {
     fn into(self) -> Vec2 {
-        Vec2::new(self.0 as f32, self.1 as f32)
+        Vec2::new((self.0 / GRID_SCALE as i32) as f32, (self.1 / GRID_SCALE as i32) as f32)
     }
 }
 
@@ -76,6 +78,7 @@ fn compute_path_to_goal(
     ) in query.iter() {
         let start_grid = GridPoint::from(Vec2::from(start.translation));
         let goal_grid = GridPoint::from(Vec2::from(goal.translation));
+        info!("start_grid: {:?}, goal_grid: {:?}", start_grid, goal_grid);
         let collider_set = QueryPipelineColliderComponentsSet(&collider_query);
 
         let result = astar(
@@ -89,8 +92,7 @@ fn compute_path_to_goal(
                         let direction: Vec2 = Mat2::from_angle(theta) * vec_position;
                         let direction = direction.normalize_or_zero();
 
-                        // unwrap since we are setting max_toi
-                        let (_, toi) = query_pipeline.cast_shape(
+                        let toi = match query_pipeline.cast_shape(
                             collider_set,
                             &start.translation.into(),
                             &direction.into(),
@@ -100,19 +102,21 @@ fn compute_path_to_goal(
                             Some(&|handle| {
                                 handle != entity.handle()
                             })
-                        ).unwrap();
-                        (position.clone() + GridPoint::from(toi.toi * direction), (toi.toi * TOI_SCALE as f32) as i32)
+                        ) {
+                            Some((_, toi)) => toi.toi,
+                            None => MAX_TOI
+                        };
+                        (position.clone() + GridPoint::from(toi * direction), toi as i32)
                     }).collect::<Vec<(GridPoint, i32)>>().into_iter()
             },
             |position| {
-                TOI_SCALE * ((position.0 - goal_grid.0).pow(2) + (position.1 - goal_grid.1).pow(2))
+                (position.0 - goal_grid.0).pow(2) + (position.1 - goal_grid.1).pow(2)
             },
             |position| *position == goal_grid
         );
 
         if let Some((path, _)) = result {
             info!("inserting path");
-
             commands.entity(entity)
                 .insert(Path {
                     points: path.iter().map(|&point| { point.into() }).collect()
@@ -126,7 +130,8 @@ fn compute_path_to_goal(
 fn draw_paths(
     mut commands: Commands,
     rc: Res<RapierConfiguration>,
-    query: Query<(Entity, &Path), Or<(Added<Path>, Changed<Path>)>>
+    query: Query<(Entity, &Path), Or<(Added<Path>, Changed<Path>)>>,
+    viz_query: Query<(Entity, &PathVisualization)>
 ) {
     for (entity, path) in query.iter() {
         info!("Draw path");
@@ -134,17 +139,23 @@ fn draw_paths(
 
         for points in path.points.windows(2) {
             if let [point1, point2] = points {
-                info!("adding points");
                 builder.add(&shapes::Line(*point1 * rc.scale, *point2 * rc.scale));
             }
         }
 
-        commands.spawn_bundle(builder
+        let line_entity = commands.spawn_bundle(builder
             .build(
                 ShapeColors::new(Color::RED),
                 DrawMode::Stroke(StrokeOptions::default().with_line_width(2.0)),
                 Transform::from_translation(Vec3::new(0.0, 0.0, 10.0))
             )
-        );
+        ).id();
+
+        if let Ok((_,PathVisualization(entity))) = viz_query.get(entity) {
+            commands.entity(*entity).despawn();
+        }
+
+        commands.entity(entity)
+            .insert(PathVisualization(line_entity));
     }
 }
