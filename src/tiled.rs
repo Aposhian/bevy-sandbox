7c8a34f7e5636d9dd2bf8c12e98062ed45778ff0
Adam Aposhian <aposhian.dev@gmail.com>
2022-01-25 18:17:39 -0700

    refactor load_texture_atlas into it's own function

--- src/tiled.rs
+++ src/tiled.rs
@@ -2,6 +2,7 @@ use bevy::prelude::*;
 use std::path::Path;
 
 use tiled::map::Map;
+use tiled::tileset::Tileset;
 
 pub struct TiledPlugin;
 
@@ -15,6 +16,24 @@ pub struct TilemapSpawnEvent {
     pub path: &'static Path,
 }
 
+fn load_texture_atlas(
+    tileset: &Tileset,
+    asset_server: &Res<AssetServer>,
+    texture_atlas_assets: &mut ResMut<Assets<TextureAtlas>>
+) {
+    if let Some(image) = &tileset.image {
+        let path = std::fs::canonicalize(&image.source).unwrap();
+        let texture_handle = asset_server.load(path);
+        let texture_atlas = TextureAtlas::from_grid(
+            texture_handle,
+            Vec2::new(tileset.tile_width as f32, tileset.tile_height as f32),
+            image.width as usize / tileset.tile_width as usize,
+            image.height as usize / tileset.tile_height as usize
+        );
+        texture_atlas_assets.add(texture_atlas);
+    }
+}
+
 /// Spawn entities in response to spawn events
 fn spawn(
     mut spawn_events: EventReader<TilemapSpawnEvent>,
@@ -24,17 +43,7 @@ fn spawn(
     for spawn_event in spawn_events.iter() {
         let map = Map::parse_file(spawn_event.path).unwrap();
         for tileset in map.tilesets {
-            if let Some(image) = tileset.image {
-                let path = std::fs::canonicalize(image.source).unwrap();
-                let texture_handle = asset_server.load(path);
-                let texture_atlas = TextureAtlas::from_grid(
-                    texture_handle,
-                    Vec2::new(tileset.tile_width as f32, tileset.tile_height as f32),
-                    image.width as usize / tileset.tile_width as usize,
-                    image.height as usize / tileset.tile_height as usize
-                );
-                texture_atlas_assets.add(texture_atlas);
-            }
+            load_texture_atlas(&tileset, &asset_server, &mut texture_atlas_assets);
         }
     }
 }
