1f7b4e6ff01a74a41c9e87ae14b86a6673ccc47d
Adam Aposhian <aposhian.dev@gmail.com>
2026-02-20 06:54:05 -0800

    claude: update win.sh

--- win.sh
+++ win.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-CARGO_FEATURES_PURE=1 cargo build --example tiled --target x86_64-pc-windows-gnu --features physics_debug --features fps_display &&
-cp ./target/x86_64-pc-windows-gnu/debug/examples/tiled.exe .
+CARGO_FEATURES_PURE=1 cargo build --example tiled --target x86_64-pc-windows-gnu "$@" &&
+cp ./target/x86_64-pc-windows-gnu/debug/examples/tiled.exe . &&
 exec ./tiled.exe
